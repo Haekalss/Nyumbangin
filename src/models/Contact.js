568328c54cf1ea019a40b4fec810136233fadb32
ferdyleoza <ferdyleoza123@gmail.com>
2025-12-15 21:34:16 +0700

    coba lagi

--- src/models/Contact.js
+++ src/models/Contact.js
@@ -36,4 +36,4 @@ const ContactSchema = new mongoose.Schema({
   timestamps: true 
 });
 
-export default mongoose.models.Contact || mongoose.model('Contact', ContactSchema);
+export default mongoose.models.Contact || mongoose.model('Contact', ContactSchema);   
